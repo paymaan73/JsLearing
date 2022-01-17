2a05bfd5212c64aa7ad73f54dad92bc62489e965
paymaan73 <paymankazemipymkz@gmail.com>
2022-01-17 20:11:10 +0330

    arithemetic_operators

---
+++ arithemetic_operators.js
@@ -0,0 +1,15 @@
+// + - / % * 
+
+var addition = 2 + 2;
+var subtraction = 3 - 90;
+var division = 10 / 5
+var multiplication = 3 * 30
+var remainder = 5 % 2
+
+
+console.log(addition)
+console.log(subtraction)
+console.log(division)
+console.log(multiplication)
+console.log(remainder)
+
