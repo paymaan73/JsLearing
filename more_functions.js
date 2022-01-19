90d9db6a552410174c8243ddc47677a6bc3e3b2f
paymaan73 <paymankazemipymkz@gmail.com>
2022-01-19 10:27:33 +0330

    more functions

---
+++ more_functions.js
@@ -0,0 +1,14 @@
+person = {name: 'Anna', age: 20}
+console.log(Object.keys(person));
+console.log(Object.values(person));
+
+console.log(typeof "James Bond");
+console.log("Paymaan".toLowerCase());
+console.log("Paymaan".endsWith("maan"));
+console.log("Paymaan".startsWith("Pay"));
+
+let str = "Hello World"
+
+for(var i = 0; i< str.length; i++){
+	console.log(str.charAt(i))
+}
