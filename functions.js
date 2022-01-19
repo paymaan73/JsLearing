73e4ba0a597833a402b321a39f5f4216688924af
paymaan73 <paymankazemipymkz@gmail.com>
2022-01-19 10:12:11 +0330

    functions

---
+++ functions.js
@@ -0,0 +1,12 @@
+function addNumbers(n1, n2){
+	// start of function body
+	// Define logic
+	// 3 + 2
+	// console.log((n1 + n2) / 2)
+	var result = n1 + n2
+	return result
+	// end of function body
+}
+
+console.log(addNumbers(12, 12));
+
