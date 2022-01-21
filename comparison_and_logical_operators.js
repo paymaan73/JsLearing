d9a81d8e345e56d95699f2850aa531e6073cf06f
paymaan73 <paymankazemipymkz@gmail.com>
2022-01-21 15:47:32 +0330

    comparison and logical operators

---
+++ comparison_and_logical_operators.js
@@ -0,0 +1,19 @@
+console.log("Comparison Operators")
+console.log(10 == 10);
+console.log(10 < 10);
+console.log(10 <= 10);
+console.log(10 > 10);
+console.log(10 >= 10);
+console.log(10 != 11);
+
+console.log("Logical Operators")
+console.log(10 == 10 && 20 == 20 && 100 == 100);
+console.log(10 == 10 && 20 != 20);
+
+
+console.log(10 == 10 || 20 == 30);
+console.log(!10 == 10);
+
+if (!(10 == 20)) {
+	console.log("if statement was executed")
+}
