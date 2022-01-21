f43470712b60c4161fbd3dec1aa536f8b470e9aa
paymaan73 <paymankazemipymkz@gmail.com>
2022-01-21 15:35:36 +0330

    break and continue

---
+++ break_and_continue.js
@@ -0,0 +1,17 @@
+var number  = 0
+/* while(number < 5){
+	console.log(number)
+	if (number == 2){
+		break;
+	}
+	number ++
+}
+*/
+while(number < 5) { 
+	number ++;
+	console.log(number)
+	if (number < 5){
+		continue;
+	}
+}
+
