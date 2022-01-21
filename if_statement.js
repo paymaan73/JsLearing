f4b62a23f9e3fd6e7b2ac524437c3cf6304f392e
paymaan73 <paymankazemipymkz@gmail.com>
2022-01-21 15:35:09 +0330

    if statement

---
+++ if_statement.js
@@ -0,0 +1,14 @@
+var person = {
+	name: 'Anna',
+	age: 19
+}
+
+
+if (person.age > 18){
+	console.log(person.name + " is an adult")
+
+} else if (person.age == 16){
+	console.log(person.name + " is about to be an adult")
+} else{
+	console.log(person.name + " is not an adult")
+}
