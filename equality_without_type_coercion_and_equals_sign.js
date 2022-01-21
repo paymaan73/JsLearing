996d6d7f45c3d5a34cb1ff4c0c3c7d0ad3c398c4
paymaan73 <paymankazemipymkz@gmail.com>
2022-01-21 18:54:30 +0330

    equality_without_type_coercion_and_equals

---
+++ equality_without_type_coercion_and_equals_sign.js
@@ -0,0 +1,7 @@
+console.log(typeof 0 + " " + typeof false)
+console.log(typeof "0" + " " + typeof false)
+
+console.log(1 == "1")
+console.log(1 === "1")
+console.log('--------------')
+console.log(typeof 1.0 +  typeof 1)
