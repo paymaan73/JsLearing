9368023c09f2f41c998d26490273f66a0223585c
paymaan73 <paymankazemipymkz@gmail.com>
2022-01-23 10:08:03 +0330

    template literals

---
+++ template_literals.js
@@ -0,0 +1,6 @@
+const name = 'Paymaan';
+const country = "Iran";
+const age = 27;
+
+console.log("Name: " + name + " Country: " + country + " age: " + age);
+console.log(`Name ${name.length} Country ${country} age ${age}`);
