baea3c106176225953a7d39220437e30d76c1280
paymaan73 <paymankazemipymkz@gmail.com>
2022-01-23 10:01:13 +0330

    const keyword

---
+++ const_keyword.js
@@ -0,0 +1,19 @@
+/* let name = 'Maria';
+name = {};
+name = 2;
+String name = 'Maria'
+name = 1 */
+
+/* const name  = 'Maria';
+name = {};
+name = 2;
+console.log(name); // Error const can not changed values
+*/
+
+const person = {};
+person['name'] = 'Maria';
+console.log(person);
+
+const array = [];
+array.push('Maria from array');
+console.log(array)
